378e61c9e7b9ea35b33992584b9f10acb6786d21
Caitlin <caitlin.smith007@gmail.com>
2015-06-21 14:22:44 -0700

    added js to make carousel disappear and static images appear when a nav link is clicked

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// Go to ariticle 1
+
+$(document).ready(function() {
+
+	// HIDE CAROUSEL AND SHOW STATIC IMAGES WHEN A NAV LINK IS CLICKED
+
+	$('#stars').click(function(){
+		$('.container-carousel').addClass('hide');
+		$('#sunImg').removeClass('show');
+		$('#eclipseImg').removeClass('show');
+		$('#shootingStarImg').addClass('show');
+	})
+
+	$('#sun').click(function(){
+		$('.container-carousel').addClass('hide');
+		$('#shootingStarImg').removeClass('show');
+		$('#eclipseImg').removeClass('show');
+		$('#sunImg').addClass('show');
+	})
+
+	$('#eclipse').click(function(){
+		$('.container-carousel').addClass('hide');
+		$('#shootingStarImg').removeClass('show');
+		$('#sunImg').removeClass('show');
+		$('#eclipseImg').addClass('show');
+	})
+});
