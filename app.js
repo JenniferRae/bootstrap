1dd5ad326f03aa2bac6cd25c575118bff2c78e10
Heflin <jenniferheflin3@gmail.com>
2015-06-22 18:17:40 -0700

    made slight changes to load faster

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
+	$( document ).ready( function() {
 	// HIDE CAROUSEL AND SHOW STATIC IMAGES WHEN A NAV LINK IS CLICKED
 
 
 	$('#starsArticle').click(function(){
 		$('.container-carousel').addClass('hide');
-		$('#sunImg #eclipseImg #shootingStarImg').removeClass('show');
+		$('#sunImg').removeClass('show');
+		$('#eclipseImg').removeClass('show');
+		$('#shootingStarImg').removeClass('show');
 		$('#one').removeClass('hide').addClass('showArticle');
 		$('#two').removeClass('showArticle').addClass('hide');
 		$('#three').removeClass('showArticle').addClass('hide');
@@ -14,7 +17,8 @@
 
 	$('#sunArticle').click(function(){
 		$('.container-carousel').addClass('hide');
-		$('#shootingStarImg #eclipseImg').removeClass('show');
+		$('#shootingStarImg').removeClass('show');
+		$('#eclipseImg').removeClass('show');
 		$('#sunImg').addClass('show');
 		$('#one').removeClass('showArticle').addClass('hide');
 		$('#two').removeClass('hide').addClass('showArticle');
@@ -23,9 +27,13 @@
 
 	$('#eclipseArticle').click(function(){
 		$('.container-carousel').addClass('hide');
-		$('#shootingStarImg #sunImg').removeClass('show');
+		$('#shootingStarImg').removeClass('show');
+		$('#sunImg').removeClass('show');
 		$('#eclipseImg').addClass('show');
 		$('#one').removeClass('showArticle').addClass('hide');
 		$('#two').removeClass('showArticle').addClass('hide');
 		$('#three').removeClass('hide').addClass('showArticle');
 	});
+
+
+});
