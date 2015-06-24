47a502717420ca83ccdc9aaba1b27bea41769912
Caitlin <caitlin.smith007@gmail.com>
2015-06-23 20:17:15 -0700

    made articles visible in large screen size

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 
-	$( document ).ready( function() {
-	// HIDE CAROUSEL AND SHOW STATIC IMAGES WHEN A NAV LINK IS CLICKED
+$( document ).ready( function() {
+	// HIDE CAROUSEL AND SHOW ARTICLES WHEN A NAV LINK IS CLICKED
 
 
 	$('#starsArticle').click(function(){
 		$('.container-carousel').addClass('hide');
-		$('#sunImg').removeClass('show');
-		$('#eclipseImg').removeClass('show');
-		$('#shootingStarImg').removeClass('show');
 		$('#one').removeClass('hide').addClass('showArticle');
 		$('#two').removeClass('showArticle').addClass('hide');
 		$('#three').removeClass('showArticle').addClass('hide');
 	});
 
-
-
 	$('#sunArticle').click(function(){
 		$('.container-carousel').addClass('hide');
-		$('#shootingStarImg').removeClass('show');
-		$('#eclipseImg').removeClass('show');
-		$('#sunImg').addClass('show');
 		$('#one').removeClass('showArticle').addClass('hide');
 		$('#two').removeClass('hide').addClass('showArticle');
 		$('#three').removeClass('showArticle').addClass('hide');
@@ -27,13 +19,24 @@
 
 	$('#eclipseArticle').click(function(){
 		$('.container-carousel').addClass('hide');
-		$('#shootingStarImg').removeClass('show');
-		$('#sunImg').removeClass('show');
-		$('#eclipseImg').addClass('show');
 		$('#one').removeClass('showArticle').addClass('hide');
 		$('#two').removeClass('showArticle').addClass('hide');
 		$('#three').removeClass('hide').addClass('showArticle');
 	});
 
+	$(window).on('resize', function(){
+      var win = $(this); 
+      if (win.width() >= 950) { 
+      	$('#one').removeClass('hide').addClass('showArticle');
+      	$('#two').removeClass('hide').addClass('showArticle');
+      	$('#three').removeClass('hide').addClass('showArticle');}
+	});
 
+	$(window).on('load', function(){
+      var win = $(this); 
+      if (win.width() >= 950) { 
+      	$('#one').removeClass('hide').addClass('showArticle');
+      	$('#two').removeClass('hide').addClass('showArticle');
+      	$('#three').removeClass('hide').addClass('showArticle');}
+	});
 });
